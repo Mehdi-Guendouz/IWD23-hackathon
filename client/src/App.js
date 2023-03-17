f213f77e24f0c03b04dc71aca7a3f52915f917bc
Guendouz mehdi <mahdign12@gmail.com>
2023-03-17 16:00:17 +0100

    setup outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import './App.scss';
-import {BrowserRouter, Route , Routes} from 'react-router-dom';
+import {BrowserRouter, Route , Routes , Outlet} from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Treatment from './components/Treatment/Treatment';
 
 
 function App() {
+
+  const Layout = () =>{
+    return(
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    )
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-      {/* <Route path="/" element={<Layout />}>
-
-      </Route> */}
-        <Route path='/' element={<Home />} />
-        {/* <Route path='/' element={<Home />} />
-        <Route path='/' element={<Home />} /> */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/content' element={<Treatment />} />
+        {/* <Route path='/' element={<Home />} /> */}
+      </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Footer from '../components/footer/Footer';
 import Hero from '../components/hero/Hero';
-import Navbar from '../components/navbar/Navbar';
 import Services from '../components/service/Services';
 import Steps from '../components/steps/Steps';
 import Treatment from '../components/Treatment/Treatment';
@@ -9,12 +7,10 @@ import Treatment from '../components/Treatment/Treatment';
 const Home = () => {
     return (
         <div>
-            <Navbar />
             <Hero />
             <Steps />
             <Treatment />
             <Services />
-            <Footer />
         </div>
     );
 }
